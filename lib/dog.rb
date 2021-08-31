4a133d399a46d3098784348da443e900eba42aed
zachlindop <85177168+zachlindop@users.noreply.github.com>
2021-08-30 18:50:23 -0700

    commit tree 872e11c0ba7bbea682f24fcfd32a46bfe447fe86

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,28 @@
 class Dog
-
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+    def breed=(breed)
+        @breed = breed
+    end
+    def breed
+        @breed
+    end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+#Define a name getter and a name= setter method for the instance variable @name.
+#Define a breed getter and a breed= setter method for the instance variable @breed.
