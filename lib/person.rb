176600ab6d67b8425d1ec3867b7ced481ad0c4c2
zachlindop <85177168+zachlindop@users.noreply.github.com>
2021-08-30 18:56:54 -0700

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,29 @@
 class Person
+    def name=(name)
+        @name=name
+    end
+    def name
+        @name
+    end
+    def job=(job)
+        @job=job
+    end
+    def job
+        @job
+    end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+#Define a name getter and a name= setter method for the instance variable @name.
+#Define a job getter and a job= setter method for the instance variable @job
